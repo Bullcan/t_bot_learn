7478231948c1f25e32d6997c38c970d2b3dbe09a
Bulat Kamalov <bulat.kamalov@icl-st.ru>
2023-10-09 09:54:55 +0300

    feet: edit some_file.py

--- some_file.py
+++ some_file.py
@@ -1,3 +1,52 @@
-print('This is a file from github repository')
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram import F
+from pprint import pprint
 
-print('These are new local changes')
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+BOT_TOKEN = '6317434316:AAHQsS5xHGkp2cpSu7-YcjIYHBehHylIIyE'
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands='start'))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+# Навешиваем декоратор с указанием в качестве фильтра типа контента
+@dp.message(F.voice)
+async def process_sent_voice(message: Message):
+    # Выводим апдейт в терминал
+    pprint(message)
+    # Отправляем сообщение в чат, откуда пришло голосовое
+    await message.answer(text='Вы прислали голосовое сообщение!')
+
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
